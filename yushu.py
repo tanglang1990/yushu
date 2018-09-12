e6b81cbe0ad5a68637a1c15e4efb837789886b8c
Ten <782555894@qq.com>
2018-09-12 16:02:58 +0800

    Response对象

--- yushu.py
+++ yushu.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -6,9 +6,28 @@ app.config.from_object('config')
 
 @app.route('/hello')
 def hello():
-    return 'hello,ten!'
+    # headers: content-type(text/html、text/plain、application/json)
+    # status code: 200 404 301
+    # body
+
+    # Response
+
+    headers = {
+        'content-type': 'text/plain',
+        'location': 'http://www.bing.com'
+    }
+    # response = make_response('<html></html>', 302)
+    # response.headers = headers
+    # return response
+
+    return '<html></html>', 302, headers
+
+    # return '<html></html>'
+
+
+def hello_func():
+    return 'hello,ten'
 
 
 if __name__ == '__main__':
-    # 生产环境是nginx+uwsgi，并不需要执行下段代码
     app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81)
