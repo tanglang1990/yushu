12a0b5148074f6265ceb19b657eda3489197b694
Ten <782555894@qq.com>
2018-09-12 17:04:54 +0800

    简单的重构及由此思考怎么去看源代码

--- yushu.py
+++ yushu.py
@@ -1,5 +1,7 @@
 from flask import Flask, make_response
 
+from helper import is_isbn_or_key
+
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -10,16 +12,7 @@ def search(q, page):
         q:普通关键字, isbn
         page:页码
     '''
-    # 判断是isbn还是关键字
-    # isbn13 由13个0-9的数字组成
-    # isbn10 由10个0-9的数字组成，可能含有'-'
-    q = q.strip()  # 去除左右的空格
-    isbn_or_key = 'key'
-    if len(q) == 13 and q.isdigit():
-        isbn_or_key = 'isbn'
-    short_q = q.replace('-', '')
-    if len(short_q) == 10 and short_q.isdigit():
-        isbn_or_key = 'isbn'
+    isbn_or_key = is_isbn_or_key
     pass
 
 
