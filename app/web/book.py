ac6e9749636447f253b72cf8d01a7f85dab647b8
Ten <782555894@qq.com>
2018-09-14 14:07:55 +0800

    Request

--- app/web/book.py
+++ app/web/book.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from flask import Blueprint
+from flask import request
 
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
@@ -7,12 +7,18 @@ from yushu_book import YuShuBook
 from . import web
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
+@web.route('/book/search')
+def search():
     '''
         q:普通关键字, isbn
         page:页码
+        /book/search?q=9787806737842&page=1
     '''
+    # Request
+    # HTTP请求信息：请求方式、参数、remote ip等等，可以看成对WSGI中environ的封装
+    q = request.args['q']
+    page = request.args['page']
+    # d = request.args.to_dict()
     isbn_or_key = is_isbn_or_key(q)
     if isbn_or_key == 'isbn':
         result = YuShuBook.search_by_isbn(q)
