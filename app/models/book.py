782c0218497bf20ff22ec9b0584432ed37fa0136
Ten <782555894@qq.com>
2018-09-14 18:03:05 +0800

    定义的一个模型类之codefirst

---
+++ app/models/book.py
@@ -0,0 +1,18 @@
+# 鸡贼的flask
+
+# sqlalchemy
+# flask_sqlalchemy
+
+# wtforms
+# flask_wtforms
+
+# 路由
+# werkzeug
+
+class Book():
+    id = None
+    title = ''
+    author = ''
+    isbn = ''
+    price = 0
+    binding = ''  # 封面
