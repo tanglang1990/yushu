e353125938bad35b60a5e1295781b1b2c879589b
Ten <782555894@qq.com>
2018-09-15 17:12:04 +0800

    flask上下文与出入栈

--- demos/demo02_working_outside_application_context.py
+++ demos/demo02_working_outside_application_context.py
@@ -1,6 +1,5 @@
 from flask import Flask, current_app, request
 
-
 app = Flask(__name__)
 
 # 应用上下文 对象 Flask
@@ -8,5 +7,7 @@ app = Flask(__name__)
 # Flask AppContext
 # Request RequestContext
 # 我们要取得对象，应该从上下文中去取
+ctx = app.app_context()
+ctx.push()
 a = current_app
 d = a.config['DEBUG']
