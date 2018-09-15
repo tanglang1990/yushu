0a29365116cbfeb1a5e0b5c4e4bf84176a594820
Ten <782555894@qq.com>
2018-09-15 14:32:50 +0800

    将模型映射到数据库中

--- app/__init__.py
+++ app/__init__.py
@@ -10,14 +10,14 @@ def create_app():
     registe_blueprint(app)
 
     '''
-        db.create_all()
+        如果只是db.create_all()
         此处会抛出 No application found的异常
         这是困扰大多数flask新手的问题
         实际上flask会去一个叫栈的地方取出flask的核心对象
-        欲知详情，请听下会讲解
+        接下来就是flask中最难理解的部分了，希望大家要有信心
     '''
     db.init_app(app)
-    db.create_all()
+    db.create_all(app=app)
     return app
 
 
