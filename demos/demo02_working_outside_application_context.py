6cc74e7e2cc74da6ea9e71bec62c7f3328703321
Ten <782555894@qq.com>
2018-09-15 15:16:46 +0800

    AppContext、RequestContext、Flask与Request之间的关系

--- demos/demo02_working_outside_application_context.py
+++ demos/demo02_working_outside_application_context.py
@@ -3,5 +3,10 @@ from flask import Flask, current_app, request
 
 app = Flask(__name__)
 
+# 应用上下文 对象 Flask
+# 请求上下文 对象 Request
+# Flask AppContext
+# Request RequestContext
+# 我们要取得对象，应该从上下文中去取
 a = current_app
 d = a.config['DEBUG']
