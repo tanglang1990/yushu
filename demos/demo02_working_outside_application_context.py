8ecfae82048aae2696e0ad243adff42986b39d9b
Ten <782555894@qq.com>
2018-09-15 17:51:02 +0800

    使用上 上下文管理器

--- demos/demo02_working_outside_application_context.py
+++ demos/demo02_working_outside_application_context.py
@@ -8,8 +8,15 @@ app = Flask(__name__)
 # Request RequestContext
 # 我们要取得对象，应该从上下文中去取
 # 离线应用 单元测试
-ctx = app.app_context()
-ctx.push()
-a = current_app
-d = a.config['DEBUG']
-ctx.pop()
+
+# ctx = app.app_context()
+# ctx.push()
+# a = current_app
+# d = a.config['DEBUG']
+# ctx.pop()
+
+
+# 上下文管理器(不懂的同学要回顾基础中上下文管理器的内容)
+with app.app_context():
+    a = current_app
+    d = a.config['DEBUG']
