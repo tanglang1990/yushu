5a606806b4db790f019644c4f25168f992a7df33
Ten <782555894@qq.com>
2018-09-18 19:44:17 +0800

    补充对上下文的说明

--- demos/demo02_working_outside_application_context.py
+++ demos/demo02_working_outside_application_context.py
@@ -20,3 +20,53 @@ app = Flask(__name__)
 with app.app_context():
     a = current_app
     d = a.config['DEBUG']
+
+
+# 文件读写
+# try:
+#     f = open(r'D:\t.txt')
+#     print(f.read())
+# finally:
+#     f.close()
+#
+# with open(r'') as f:
+#     print(f.read())
+# 1. 连接数据库
+# 2. sql
+# 3. 释放资源
+#
+# __enter__
+# __exit__
+# try
+# except
+# finally:
+
+# 实现了上下文协议的对象使用with
+# 上下文管理器
+# __enter__ __exit__
+# 上下文表达式必须要返回一个上下文管理器
+# with
+
+
+class MyResource:
+    def __enter__(self):
+        print('connect to resource')
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        if tb:
+            print('process exception')
+        else:
+            print('no exception')
+        print('close resource connection')
+
+    def query(self):
+        print('query data')
+
+
+try:
+    with MyResource() as resource:
+        1 / 0
+        resource.query()
+except Exception as ex:
+    pass
