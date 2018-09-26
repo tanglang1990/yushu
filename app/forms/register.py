fdac27e653ba338f3c47c8088f03b51b929ed0e5
Ten <782555894@qq.com>
2018-09-26 17:23:20 +0800

    自定义验证器

--- app/forms/register.py
+++ app/forms/register.py
@@ -1,5 +1,7 @@
 from wtforms import Form, StringField, PasswordField
-from wtforms.validators import Length, DataRequired, Email
+from wtforms.validators import Length, DataRequired, Email, ValidationError
+
+from app.models.user import User
 
 
 class RegisterForm(Form):
@@ -11,3 +13,11 @@ class RegisterForm(Form):
 
     nickname = StringField(validators=[
         DataRequired(), Length(2, 10, message='昵称至少需要两个字符，最多10个字符')])
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('该电子邮件已被注册')
+
+    def validate_nickname(self, field):
+        if User.query.filter_by(nickname=field.data).first():
+            raise ValidationError('昵称已存在')
