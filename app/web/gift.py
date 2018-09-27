9e03686520bb7e37687a6220a384b941c83a7353
Ten <782555894@qq.com>
2018-09-27 17:59:25 +0800

    增加跳转

--- app/web/gift.py
+++ app/web/gift.py
@@ -1,4 +1,4 @@
-from flask import current_app, flash
+from flask import current_app, flash, redirect, url_for
 from flask_login import login_required, current_user
 
 from app.models.base import db
@@ -31,6 +31,7 @@ def save_to_gifts(isbn):
         #     raise e
     else:
         flash('这本书已添加至你的赠送清单或已存在于你的心愿清单，请不要重复添加')
+    return redirect(url_for('web.book_detail', isbn=isbn))
 
 
 @web.route('/gifts/<gid>/redraw')
