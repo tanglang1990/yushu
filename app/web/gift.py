888c72145fbbc4674895a9917cc3e2ad29b48455
Ten <782555894@qq.com>
2018-09-27 16:09:18 +0800

    事务与回滚

--- app/web/gift.py
+++ app/web/gift.py
@@ -24,8 +24,9 @@ def save_to_gifts(isbn):
             current_user.beans += current_app.config['BEANS_UPLOAD_ONE_BOOK']
             db.session.add(gift)
             db.session.commit()
-        except Exception:
+        except Exception as e:
             db.session.rollback()
+            raise e
     else:
         flash('这本书已添加至你的赠送清单或已存在于你的心愿清单，请不要重复添加')
 
