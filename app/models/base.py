99962de59c284fb65a9b6db32a43ffc7a6b610cb
Ten <782555894@qq.com>
2018-09-27 17:49:54 +0800

    修复模型Base中的一个坑，添加create_time字段，此处需留意类变量的陷阱

--- app/models/base.py
+++ app/models/base.py
@@ -1,15 +1,16 @@
 from contextlib import contextmanager
+from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy
-from sqlalchemy import Column, SmallInteger
+from sqlalchemy import Column, SmallInteger, Integer
 
 
 class SQLAlchemy(_SQLAlchemy):
     @contextmanager
     def auto_commit(self):
         try:
-            db.session.commit()
             yield
+            db.session.commit()
         except Exception as e:
             db.session.rollback()
             raise e
@@ -20,7 +21,16 @@ db = SQLAlchemy()
 
 class Base(db.Model):
     __abstract__ = True
+    # 此处有坑
+    # 如果在类的create_time中增加default=int(datetime.now().timestamp()
+    # 那么在所有数据库中的记录时间会是一样的
+    # 这是由于类的类变量和实例变量的很重要的区别
+    # 类变量的定义是发生在类的定义的过程中，而不是实例的初始化过程中
+    create_time = Column('create_time', Integer)
     status = Column(SmallInteger, default=1)
+
+    def __init__(self):
+        self.create_time = int(datetime.now().timestamp())
 
     def set_attrs(self, attrs_dict):
         for key, value in attrs_dict.items():
