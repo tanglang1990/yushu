201b08c1955ca5b33c352f05b03074c7b80bf445
Ten <782555894@qq.com>
2018-09-28 15:04:23 +0800

    处理时间，把模型中的时间戳转换为datetime

--- app/models/base.py
+++ app/models/base.py
@@ -36,3 +36,10 @@ class Base(db.Model):
         for key, value in attrs_dict.items():
             if hasattr(self, key) and key not in ['id']:
                 setattr(self, key, value)
+
+    @property
+    def create_datetime(self):
+        if self.create_time:
+            return datetime.fromtimestamp(self.create_time)
+        else:
+            return None
