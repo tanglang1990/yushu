2c360d7c38b88fe1e5b6dfc3d6365d76140018a1
Ten <782555894@qq.com>
2018-09-28 22:15:32 +0800

    用户注销

--- app/web/auth.py
+++ app/web/auth.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 from app.forms.auth import RegisterForm, LoginForm
 from app.models.base import db
@@ -65,4 +65,5 @@ def change_password():
 
 @web.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('web.index'))
