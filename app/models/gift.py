8518863dcbc6fb0dabce9b517acf23dfc6882fbe
Ten <782555894@qq.com>
2018-09-28 17:02:32 +0800

    最近的礼物(复杂SQL在orm中的实现)以及链式调用

--- app/models/gift.py
+++ app/models/gift.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String
+from flask import current_app
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc
 from sqlalchemy.orm import relationship
 
 from app.models.base import Base
@@ -12,3 +13,16 @@ class Gift(Base):
     # book = relationship('Book') # 由于我们的book是一张空表，故这里只记录上面的isbn号即可
     # bid = Column(Integer, ForeignKey('book.id'))
     launched = Column(Boolean, default=False)
+
+    @classmethod
+    def recent(cls):
+        # 链式调用
+        # 主体 Query
+        # 子函数
+        # first() all()
+        recent_gift = cls.query.filter_by(
+            launched=False).group_by(
+            cls.isbn).order_by(
+            desc(cls.create_time)).limit(
+            current_app.config['RECENT_BOOK_COUNT']).distinct().all()
+        return recent_gift
