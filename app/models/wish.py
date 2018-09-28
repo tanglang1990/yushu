50e5bb1dc414eb59a45a30cc580c0fdbdee244b1
Ten <782555894@qq.com>
2018-09-28 23:31:46 +0800

    我的心愿

--- app/models/wish.py
+++ app/models/wish.py
@@ -1,7 +1,10 @@
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, desc, func
 from sqlalchemy.orm import relationship
 
-from app.models.base import Base
+from app.models.base import Base, db
+from app.models.gift import Gift
+from app.spider.yushu_book import YuShuBook
+from app.view_models.book import BookViewModel
 
 
 class Wish(Base):
@@ -10,3 +13,27 @@ class Wish(Base):
     uid = Column(Integer, ForeignKey('user.id'))
     isbn = Column(String(15), nullable=False)
     launched = Column(Boolean, default=False)
+
+    @property
+    def book(self):
+        data = YuShuBook.search_by_isbn(self.isbn)
+        book = BookViewModel.handle_book_data(data)
+        return book
+
+    @classmethod
+    def get_user_wishes(cls, uid):
+        gifts = Wish.query.filter_by(uid=uid, launched=False).order_by(
+            desc(Wish.create_time)).all()
+        return gifts
+
+    @classmethod
+    def get_gift_counts(cls, isbn_list):
+        # 输入：['9787108006721', '9787544247252']
+        # 输出：{'9787108006721':1, '9787544247252':2}]
+        count_list = db.session.query(func.count(Gift.id), Gift.isbn).filter(
+            Gift.launched == False,
+            Gift.isbn.in_(isbn_list),
+            Gift.status == 1).group_by(
+            Gift.isbn).all()
+        count_dict = {w[1]: w[0] for w in count_list}
+        return count_dict
--- app/web/wish.py
+++ app/web/wish.py
@@ -1,15 +1,23 @@
-from flask import flash, redirect, url_for
+from flask import flash, redirect, url_for, render_template
 from flask_login import login_required, current_user
 
 from app.models.base import db
 from app.models.wish import Wish
+from app.view_models.wish import MyWishes
 
 from . import web
 
 
 @web.route('/my/wish')
+@login_required
 def my_wish():
-    pass
+    uid = current_user.id
+    wishes_of_mine = Wish.get_user_wishes(uid)
+    isbn_list = [wish.isbn for wish in wishes_of_mine]
+    gift_count_list = Wish.get_gift_counts(isbn_list)
+
+    view_model = MyWishes(wishes_of_mine, gift_count_list)
+    return render_template('my_wish.html', wishes=view_model.gifts)
 
 
 @web.route('/wish/book/<isbn>')
