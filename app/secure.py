013e14c2a0564d5c6e3e2bdbfd21675571f36a5b
Ten <782555894@qq.com>
2018-10-02 19:49:14 +0800

    鱼漂业务逻辑与Drift模型(合理利用数据冗余记录历史状态)

--- app/secure.py
+++ app/secure.py
@@ -4,16 +4,9 @@ SECRET_KEY = '\xefa\xc6\xf9\xa4-\xf8X\xd3\xb4c4\xefr\x1b\xbd\x9b\xc9\xa1ua%Nn'
 CACHE_TYPE = 'simple'
 
 # https://service.mail.qq.com/cgi-bin/help?subtype=1&&id=28&&no=369
-# MAIL_SERVER = 'smtp.qq.com'
-# MAIL_PORT = '465'
-# MAIL_USE_TLS = False
-# MAIL_USE_SSL = True
-# MAIL_USERNAME = 'demo@qq.com'
-# MAIL_PASSWORD = 'authcode'
-
 MAIL_SERVER = 'smtp.qq.com'
 MAIL_PORT = '465'
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = '782555894@qq.com'
-MAIL_PASSWORD = 'efgylnhjzrnvbchb'
+MAIL_USERNAME = 'demo@qq.com'
+MAIL_PASSWORD = 'authcode'
