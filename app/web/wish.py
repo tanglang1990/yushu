5882fb1d86e9f72d9b10e365de2012a40d60b5ce
Ten <782555894@qq.com>
2018-10-03 11:13:10 +0800

    向他人赠送书籍

--- app/web/wish.py
+++ app/web/wish.py
@@ -1,7 +1,10 @@
-from flask import flash, redirect, url_for, render_template
+from flask import flash, redirect, url_for, render_template, request
 from flask_login import login_required, current_user
+from werkzeug.contrib import limiter
 
+from app.libs.emailer import send_mail
 from app.models.base import db
+from app.models.gift import Gift
 from app.models.wish import Wish
 from app.view_models.trade import MyTrades
 from app.view_models.wish import MyWishes
@@ -36,8 +39,19 @@ def save_to_wish(isbn):
 
 
 @web.route('/satisfy/wish/<int:wid>')
+@login_required
 def satisfy_wish(wid):
-    pass
+    wish = Wish.query.get_or_404(wid)
+    gift = Gift.query.filter_by(uid=current_user.id, isbn=wish.isbn).first()
+    if not gift:
+        flash('你还没有上传此书，'
+              '请点击“加入到赠送清单”添加此书。添加前，请确保自己可以赠送此书')
+    else:
+        send_mail(wish.user.email,
+                  '有人想送你一本书', 'email/satisify_wish.html', wish=wish,
+                  gift=gift)
+        flash('已向他/她发送了一封邮件，如果他/她愿意接受你的赠送，你将收到一个鱼漂')
+    return redirect(url_for('web.book_detail', isbn=wish.isbn))
 
 
 @web.route('/wish/book/<isbn>/redraw')
